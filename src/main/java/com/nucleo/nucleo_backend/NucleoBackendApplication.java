3223b5b6640878fccb77ebc3cb651eb48db1e5d7
GCastroGarcia <Lobeno6920@gmail.com>
2026-08-30 21:46:05 -0500

    first commit

---
+++ src/main/java/com/nucleo/nucleo_backend/NucleoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nucleo.nucleo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucleoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NucleoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nucleo/nucleo_backend/NucleoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nucleo.nucleo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NucleoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
